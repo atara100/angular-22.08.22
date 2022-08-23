6cae74058d739bba2e0e88b40de506df6c0e6ee8
atara100 <al0583230109@gmail.com>
2022-08-24 00:10:38 +0300

    button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent } from './main/main.component';
 import { TitleComponent } from './title/title.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [  
-    MainComponent, TitleComponent
+    MainComponent, TitleComponent, ButtonComponent
   ],
   imports: [
     BrowserModule
